2572af948dc75f7d3c68077e4193078ce49a5d45
Joey <joeyskuo@gmail.com>
2020-02-17 10:44:00 -0800

    validation for post length added

--- routes/board.js
+++ routes/board.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const { body } = require('express-validator/check');
 
 const boardController = require('../controllers/board');
 
 const router = express.Router();
 
 router.get('/posts', boardController.getPosts);
-router.post('/post', boardController.createPost);
+router.post('/post', [
+            body('title').trim().isLength({min: 5}),
+            body('content').trim().isLength({min: 5})
+        ], 
+        boardController.createPost);
 
 module.exports = router;
