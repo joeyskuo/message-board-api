f053d60cf0a7437f71a63088ef6e7f23c5e80706
Joey <joeyskuo@gmail.com>
2020-02-17 21:07:13 -0800

    imageUrl in create-post from request body

--- controllers/board.js
+++ controllers/board.js
@@ -26,12 +26,19 @@ exports.getPosts = (req, res, next) => {
       throw error;
     }
 
+    if(!req.file) {
+      const error = new Error('No image provided');
+      error.statusCode = 422;
+      throw error;
+    }
+    const imageUrl = req.file.path;
+
     const title = req.body.title;
     const content = req.body.content;
     const post = new Post({
       title: title,
       content: content,
-      imageUrl: 'logo.jpg',
+      imageUrl: imageUrl,
       creator: {
         name: 'User1'
       }
