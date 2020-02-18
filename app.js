796ab8f58ac772b09fe40d5fcfc6f1187086b67d
Joey <joeyskuo@gmail.com>
2020-02-17 20:46:11 -0800

    multer configured and added as middleware

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const multer = require('multer');
 const mongoose = require('mongoose');
 const db = require('./private/credentials');
 
@@ -8,8 +9,30 @@ const boardRoutes = require('./routes/board');
 
 const app = express();
 
+const fileStorage = multer.diskStorage( {
+    destination: (req, file, cb) => {
+        cb(null, 'images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, new Date().toISOString() + '-' + file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if(
+        file.mimetype === 'image/png' ||
+        file.mimetype === 'image/jpg' ||
+        file.mimetype === 'image/jpeg'
+    ) {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+}
+
 // Middleware
 app.use(bodyParser.json()); 
+app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'));
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
